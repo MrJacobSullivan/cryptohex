b8201c8bd1cca268cbfbb5c655cabbe6e49b2762
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-11-24 02:29:56 -0600

    added routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Color from './pages/Color';
+
 const App = () => {
   return (
-    <div>
-      <p>Hello, world.</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:hex" element={<Color />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
